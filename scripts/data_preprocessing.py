3f7dd86cf317525583a5e30197d3d7311f3895ac
michaelgetachew <mikygetyos@gmail.com>
2022-09-06 23:46:37 +0300

    Feat(add): added all the functions for preprocessing with thier logging

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -27,3 +27,54 @@ class data_preprocessor:
         data_types = self.df.dtypes
         self.logger.info(f"Data Types: {data_types}")
         return self.df.dtypes
+
+    def show_data_description(self) -> pd.DataFrame:
+        description = self.df.describe()
+        self.logger.info(f"Description : {description}")
+        return self.df.describe()
+
+    def show_data_info(self) -> pd.DataFrame:
+        self.logger(f"Info: {self.df.info()}")
+        return self.df.info()
+
+    def show_statistical_info(self) -> pd.DataFrame:
+        self.logger.info(f"Showing statistical info")
+        return self.df.agg(['mean'])
+
+    def show_correlation(self) -> pd.DataFrame:
+        self.logger.info(f"Showing correlation")
+        return self.df.corr()
+
+    def collective_grouped_mean(self, colomnName: str) -> pd.DataFrame:
+        groupby_colomnName = self.df.groupby(colomnName)
+        self.logger.info(f"Collective grouped mean")
+        return groupby_colomnName.mean()
+
+    def list_coloumn_names(self) -> pd.DataFrame:
+        self.logger.info(f"Showing coloumn names")
+        return self.df.columns
+
+    def colums_WithMissingValue(self):
+        miss = []
+        dff = self.df.isnull().any()
+        summ = 0
+        for col in dff:
+            if col == True:
+                miss.append(dff.index[summ])
+            summ += 1
+        self.logger.info(f"Colums with missing values: {miss}")
+        return miss
+
+    def get_column_based_missing_percentage(self):
+        col_null = self.df.isnull().sum()
+        total_entries = self.df.shape[0]
+        missing_percentage = []
+        for col_missing_entries in col_null:
+            value = str(
+                round(((col_missing_entries/total_entries) * 100), 2)) + " %"
+            missing_percentage.append(value)
+
+        missing_df = pd.DataFrame(col_null, columns=['total_missing_values'])
+        missing_df['missing_percentage'] = missing_percentage
+        self.logger.info(f"Showing missing percentage")
+        return missing_df
