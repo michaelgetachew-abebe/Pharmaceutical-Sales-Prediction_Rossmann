3885092f433efa4f36bb917980338762cdff202b
michaelgetachew <mikygetyos@gmail.com>
2022-09-06 23:58:06 +0300

    Feat(add): added a constructot to the data_cleaner class

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import numpy as np
 from logger_creator import logwritter
+
+class data_cleaner:
+
+    def __init__(self, df: pd.DataFrame, deep=False) -> None:
+        
+        self.logger = logwritter(
+            "../logs/data_cleaner_log.log").get_logwritter()
+        if(deep):
+            self.df = df.copy(deep=True)
+        else:
+            self.df = df
+
+    
