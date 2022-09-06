779d85c69f4b8342539d6ad17d8b56455b2f2299
michaelgetachew <mikygetyos@gmail.com>
2022-09-06 23:33:43 +0300

    Feat(add) added additional functions for preprocessing

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,12 +1,27 @@
+from cmath import log
 import os,sys
 
 import pandas as pd
 import numpy as np
 
+from logger_creator import logwritter
+# Create an instance of the logwriter class
+
+#log_writter = logwritter("logs/data_preprocessing_logs.log").get_logwritter()
+
 sys.path.append(os.path.abspath(os.path.join('../logs')))
+
 
 class data_preprocessor:
 
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
-        
+        self.logger = logwritter("logs/data_preprocessing_log.log").get_logwritter()
+
+    def drop_duplicates(self) -> pd.DataFrame:
+        dropped = self.df[self.df.duplicated()].index
+        self.logger.info(f"Dropping duplicates: {dropped}")
+        return self.df.drop(index=dropped, inplace=True)
+
+    def see_datatypes(self) -> pd.DataFrame:
+        self.logger.info("S")
