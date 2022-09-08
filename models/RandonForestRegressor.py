424222328b94f539c47c8b1f96bac040d8eb5dd5
michaelgetachew <mikygetyos@gmail.com>
2022-09-08 12:12:52 +0300

    Feat(add): done the necessary import Pkgs

--- models/RandonForestRegressor.py
+++ models/RandonForestRegressor.py
@@ -1,9 +1,19 @@
-import sys
 import pandas as pd
 import numpy as np
 import dvc.api
 import mlflow
-from sklearn.ensemble import RandomForestRegressor
-#import mlflow.sklearn
+import mlflow.sklearn
+import logging
+import pickle
 import warnings
+warnings.filterwarnings('ignore')
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import *
 
+import os
+import sys
+cwd = os.getcwd()
+sys.path.append(f"{cwd}/scripts")
+
+from data_preprocess import preprocess
+from logger_creator import log
