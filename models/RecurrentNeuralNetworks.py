2db4ba1a6c0b9cb98c82d2956ad6983efbc938d9
michaelgetachew <mikygetyos@gmail.com>
2022-09-09 16:18:33 +0300

    Feat(modify): started modification of the random forest file

--- models/RecurrentNeuralNetworks.py
+++ models/RecurrentNeuralNetworks.py
@@ -9,3 +9,13 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
+
+time_series_data = pd.read_csv('../data/train_store.csv',
+                               index_col="Date", parse_dates=True)
+time_series_data = time_series_data['Sales']
+time_series_data = pd.DataFrame({
+    "Date": time_series_data.index,
+    "Sales": time_series_data
+})
+time_series_data = time_series_data.set_index("Date")
+print(time_series_data.head())
