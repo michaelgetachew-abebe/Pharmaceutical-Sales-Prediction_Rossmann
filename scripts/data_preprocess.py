53009afb8d5816ec42e9abba5424935c731d25df
michaelgetachew <mikygetyos@gmail.com>
2022-09-10 01:34:28 +0300

    Feat(add): added outlier replacer

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -129,3 +129,19 @@ class data_preprocess:
         for col in columns:
             df[col] = df[col].fillna(0)
         return df
+
+    def replace_outliers_iqr(self, df, columns):
+        
+        for col in columns:
+            Q1, Q3 = df[col].quantile(
+                0.25), df[col].quantile(0.75)
+            IQR = Q3 - Q1
+            cut_off = IQR * 1.5
+            lower, upper = Q1 - cut_off, Q3 + cut_off
+
+            df[col] = np.where(
+                df[col] > upper, upper, df[col])
+            df[col] = np.where(
+                df[col] < lower, lower, df[col])
+        return df
+    
